11db06e15348a2c78ab15efe0eaadd2dd4a24c0c
Vaidehi Deo <vaidehideo@gmail.com>
2015-04-22 10:34:00 -0700

    Added Album metadata popup

--- src/main/resources/static/fba.js
+++ src/main/resources/static/fba.js
@@ -40,4 +40,59 @@ app.controller('myCtrl', function($scope,$http) {
         });
     }
 
+    $scope.showModal = false;
+    $scope.toggleModal = function(val){
+        $scope.album_id = val;
+        $scope.showModal = !$scope.showModal;
+        $http({
+            method :'GET',
+            url : '/getalbummeta?album_id='+ $scope.album_id
+        }).success(function(response){
+            $scope.albums = response;
+            return /*JSON.stringify(*/response/*,null,'\t')*/;
+        })
+    };
+});
+
+
+app.directive('modal', function () {
+    return {
+        template: '<div class="modal fade">' +
+        '<div class="modal-dialog">' +
+        '<div class="modal-content">' +
+        '<div class="modal-header">' +
+        '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+        '<h4 class="modal-title">{{ title }}</h4>' +
+        '</div>' +
+        '<div class="modal-body" ng-transclude></div>' +
+        '</div>' +
+        '</div>' +
+        '</div>',
+        restrict: 'E',
+        transclude: true,
+        replace:true,
+        scope:true,
+        link: function postLink(scope, element, attrs) {
+            scope.title = attrs.title;
+
+            scope.$watch(attrs.visible, function(value){
+                if(value == true)
+                    $(element).modal('show');
+                else
+                    $(element).modal('hide');
+            });
+
+            $(element).on('shown.bs.modal', function(){
+                scope.$apply(function(){
+                    scope.$parent[attrs.visible] = true;
+                });
+            });
+
+            $(element).on('hidden.bs.modal', function(){
+                scope.$apply(function(){
+                    scope.$parent[attrs.visible] = false;
+                });
+            });
+        }
+    };
 });
