2de3d6d3ca143cb2da9b4125c8e529fddffc1a85
Vaidehi Deo <vaidehideo@gmail.com>
2015-04-22 11:16:12 -0700

    Fixed merging errors

--- src/main/resources/static/fba.js
+++ src/main/resources/static/fba.js
@@ -4,7 +4,28 @@ app.controller('myCtrl', function ($scope, $http) {
 	$scope.dispVal = 'false';
 	$scope.detailDispVal = 'false';
 
-<<<<<<< HEAD
+    $scope.getAlbums = function () {
+        $scope.dispVal = 'true';
+        $http({
+            method: 'GET',
+            url: '/getalbums'
+        }).success(function (response) {
+            $scope.albums = response;
+            return response;
+        })
+    }
+
+    $scope.getPhotos = function (val) {
+        $scope.album_id = val;
+        $http({
+            method: 'GET',
+            url: '/' + $scope.album_id + '/photos'
+        }).success(function (response) {
+            $scope.photos = response;
+            return;
+        });
+    }
+
     $scope.showModal = false;
     $scope.toggleModal = function(val){
         $scope.album_id = val;
@@ -17,8 +38,27 @@ app.controller('myCtrl', function ($scope, $http) {
             return /*JSON.stringify(*/response/*,null,'\t')*/;
         })
     };
-});
 
+    $scope.backupAlbum = function (val) {
+        $scope.album_id = val;
+        $http({
+            method: 'GET',
+            url: '/backup?album_id=' + $scope.album_id
+        }).success(function (response) {
+            $scope.status = response;
+            if ($scope.status === null) {
+                alert("Album " + $scope.status._id + " is successfully backed up!");
+            } else {
+                alert("Sorry, Backup failed");
+            }
+            return status;
+        });
+    }
+
+    $scope.getAlbumMeta = function () {
+        $scope.detailDispVal = 'true';
+    }
+});
 
 app.directive('modal', function () {
     return {
@@ -61,49 +101,3 @@ app.directive('modal', function () {
         }
     };
 });
-=======
-	$scope.getAlbums = function () {
-		$scope.dispVal = 'true';
-		$http({
-			method: 'GET',
-			url: '/getalbums'
-		}).success(function (response) {
-			$scope.albums = response;
-			return response;
-		})
-	}
-
-	$scope.getPhotos = function (val) {
-		$scope.album_id = val;
-		$http({
-			method: 'GET',
-			url: '/' + $scope.album_id + '/photos'
-		}).success(function (response) {
-			$scope.photos = response;
-			return;
-		});
-	}
-
-	$scope.backupAlbum = function (val) {
-		$scope.album_id = val;
-		$http({
-			method: 'GET',
-			url: '/backup?album_id=' + $scope.album_id
-		}).success(function (response) {
-			$scope.status = response;
-			if ($scope.status === null) {
-				alert("Album " + $scope.status._id + " is successfully backed up!");
-			} else {
-				alert("Sorry, Backup failed");
-			}
-			return status;
-		});
-	}
-
-	$scope.getAlbumMeta = function () {
-		$scope.detailDispVal = 'true';
-	}
-
-
-});
->>>>>>> origin/master
