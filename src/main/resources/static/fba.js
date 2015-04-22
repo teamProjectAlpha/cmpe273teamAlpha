ab3ca4b054b4eecdc5096f196a323a4773c2ce23
Ashish Bende <eashishbende@gmail.com>
2015-04-22 03:56:54 -0700

    update
    added  fba script functionality.
    Fixed styles and other errors.

--- src/main/resources/static/fba.js
+++ src/main/resources/static/fba.js
@@ -1,43 +1,50 @@
 var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope,$http) {
-    $scope.firstName= "Jomndfbdhn";
-    $scope.lastName= "Doe";
+app.controller('myCtrl', function ($scope, $http) {
 
-    $scope.getAlbums = function(){
-        $http({
-            method :'GET',
-            url : '/getalbums'
-        }).success(function(response){
-            $scope.albums = response;
-            return /*JSON.stringify(*/response/*,null,'\t')*/;
-        })
-    }
-    
-    $scope.getPhotos = function(val){
-        $scope.album_id = val;
-        $http({
-            method: 'GET',
-            url: '/'+$scope.album_id+'/photos'
-        }).success(function(response){
-            $scope.photos = response;
-            return;
-        });
-    }
-    
-    $scope.backupAlbum = function(val){
-        $scope.album_id = val;
-        $http({
-            method: 'GET',
-            url: '/backup?album_id='+ $scope.album_id
-        }).success(function(response){
-            $scope.status = response;
-            if($scope.status === null){
-                alert("Backup failed");
-            }else{
-                alert("Album "+$scope.status._id +" is successfully backed up!");
-            }
-            return status;
-        });
-    }
+	$scope.dispVal = 'false';
+	$scope.detailDispVal = 'false';
+
+	$scope.getAlbums = function () {
+		$scope.dispVal = 'true';
+		$http({
+			method: 'GET',
+			url: '/getalbums'
+		}).success(function (response) {
+			$scope.albums = response;
+			return response;
+		})
+	}
+
+	$scope.getPhotos = function (val) {
+		$scope.album_id = val;
+		$http({
+			method: 'GET',
+			url: '/' + $scope.album_id + '/photos'
+		}).success(function (response) {
+			$scope.photos = response;
+			return;
+		});
+	}
+
+	$scope.backupAlbum = function (val) {
+		$scope.album_id = val;
+		$http({
+			method: 'GET',
+			url: '/backup?album_id=' + $scope.album_id
+		}).success(function (response) {
+			$scope.status = response;
+			if ($scope.status === null) {
+				alert("Album " + $scope.status._id + " is successfully backed up!");
+			} else {
+				alert("Sorry, Backup failed");
+			}
+			return status;
+		});
+	}
+
+	$scope.getAlbumMeta = function () {
+		$scope.detailDispVal = 'true';
+	}
+
 
 });
